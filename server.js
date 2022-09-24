7491ae6f836d23b7a7c13faecfe9c0c81eec9656
playmaker159 <ashwinprabhu2001@gmail.com>
2022-09-24 19:47:34 +0530

    socket io stock application

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.use(express.static((__dirname)))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// io.on('connection', (socket) => {
+//     console.log("User connected");
+//     socket.broadcast.emit('hi')
+
+//     socket.on('chat message', (msg) => {
+//       console.log('message: ' + msg);
+//       socket.broadcast.emit(msg)
+//     });
+//     socket.on('disconnect',()=>{
+//         console.log("User disconnected");
+//     })
+//   });
+
+io.on('connection', (socket) => {
+
+    socket.on('new user',(user)=>{
+        console.log('new user: ' + user)
+        io.emit('new user',user);
+    })
+
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+      console.log(msg);
+    });
+  });
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
